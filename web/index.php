879d476b82542115c6b74c527f36c552d5206967
Roland Kullet <renaud.bellec.3@gmail.com>
2015-12-16 17:36:20 +0100

    web/ started workin on timeline html

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" type="text/css" href="./css/grid.css">
     <link rel="stylesheet" type="text/css" href="./css/global.css">
     <link rel="stylesheet" type="text/css" href="./css/icomoon.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <header>
@@ -13,6 +14,7 @@
                 <img src="./img/logo-pa.png" alt="Play Now" class="app-logo" height="100px" width="92px">
             </div>
             <div class="col-xs-9">
+                <div class="header-separator"></div>
                 <a href="#" class="icon">
                     <span class="icon-location"></span>
                 </a>
@@ -35,6 +37,41 @@
         </div>
     </div>
 </header>
+<section class="timeline">
+    <article class="grid-container event">
+        <div class="row">
+            <div class="col-xs-2">
+                <img src="./img/user-icon.png" alt="User Picture" class="user-picture">
+            </div>
+            <div class="col-xs-10">
+                <div class="row">
+                    <div class="col-xs-8">
+                        <h2 class="title-box">
+                            Event title
+                            <span class="location">
+                                <span class="icon-location"></span> Toulouse
+                            </span>
+                        </h2>
+                    </div>
+                    <div class="col-xs-4">
+                        <h4>Nb Users</h4>
+                    </div>
+                </div>
+                <div class="event-description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum sodales ante at sagittis. Morbi vitae nisi quis lorem fringilla hendrerit id vel ante. Ut justo tortor, sodales nec tempus vel, viverra sodales sapien. Integer bibendum lacus sed dignissim porttitor. Nulla a molestie lorem. Nulla quis condimentum augue. Sed tincidunt tristique lectus sit amet convallis.</p>
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <p>Event icons and tags</p>
+                    </div>
+                    <div class="col-xs-4">
+                        <p>Event Actions</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
 <div class="grid-container">
     
 </div>
