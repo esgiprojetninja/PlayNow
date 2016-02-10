a1383df945277cb0165ec50f974e0aef9643b32f
Florian Meyronneinc <florianmeyronneinc@MacBook-Pro-de-Florian.local>
2016-02-10 12:40:46 +0100

    To do

--- app/configurations/Configuration.php
+++ app/configurations/Configuration.php
@@ -4,11 +4,8 @@ namespace Playnow\app\configurations;
 
 class Configuration implements ConfigurationInterface
 {
-<<<<<<< HEAD
-    public function __construct($arrayConfig) {
+    public function __construct($arrayConfig)
+    {
 
     }
-=======
-    
->>>>>>> aacc8f52c84497b50e329842ac2bb2910187cf06
 }
--- app/router/Route.php
+++ app/router/Route.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * @nicoto @rolandkuku :
+ * Créer une méthode group qui sera appellée avec Route::group permettant de générer une collection de routes
+ * Rajouter la méthode avec les bons paramètres dans l'interface
+ * Plusieurs group possibles : Group par namespace, Group par controller, Group par prefix.
+ *
+ * @florian @gorfy :
+ * Créer les méthodes get, post, put, patch, delete
+ * Rajouter les méthodes avec les bons paramètres dans l'interface
+ * Gérer les paramètres envoyés dans l'url de type : {id}
+ * Gérer les paramètres optionnels envoyé dans l'url de type : {name?}
+ *
+ * @Everyone :
+ * Créer la methode match qui permet de matcher en fonction de l'url
+ * Créer la méthode any qui permet d'ajouter des paramètres à toutes les routes
+ * Rajouter les méthodes dans l'interface avec les bons paramètres
+ */
+
 namespace Playnow\app\router;
 
 class Route implements RouteInterface
