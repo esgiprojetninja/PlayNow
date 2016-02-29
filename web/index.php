1070afbca87eeeaee220daa2b5b8e8e4e4fdd58a
Roland Kullet <renaud.bellec.3@gmail.com>
2016-02-29 19:35:48 +0100

    fixed landing page

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PLAY NOW | GET WET MOTHERFUCKERS !</title>
+    <title>PLAY NOW</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="./css/grid.css">
     <link rel="stylesheet" type="text/css" href="./css/global.css">
@@ -14,8 +14,34 @@
 <body>
     <div id="fixed-container">
         <div id="slide1" class="landing-slide">
+            <div class="landing-text">
+                <h3>
+                    Organisez <span class="strong">des matchs</span> en 2 clics !
+                </h3>
+                <p>
+                    Avec <span class="strong">Play Now</span> vous pouvez organiser un match sur un coup de tête. Faites savoir où et quand vous voulez jouer aux joueurs autour de vous ! <span class="strong">Validez</span> et c'est parti !
+                </p>
+            </div>
         </div>
         <div id="slide2" class="landing-slide">
+            <div class="landing-text">
+                <h3>
+                    Trouvez des <span class="strong">joueurs</span> partout, tout le temps !
+                </h3>
+                <p>
+                    <span class="strong">Play Now</span> vous permet de rechercher et trouver des parties de sport autour de vous. Plus besoin de s'y prendre des semaines à l'avance ! Connectez vous, et <span class="strong">bougez</span> !
+                </p>
+            </div>
+        </div>
+        <div id="slide3" class="landing-slide">
+            <div class="landing-text">
+                <h3>
+                    Profitez d'une <span class="strong">communauté</span> qui vous ressemble !
+                </h3>
+                <p>
+                    <span class="strong">Play Now</span> connecte les sportifs autour de vous. Rencontrez-les autour de votre passion commune et dépassez vous <span class="strong">ensemble</span> ! Vous ne manquerez plus jamais de <span class="strong">partenaires</span> !
+                </p>
+            </div>
         </div>
         <div id="device-container">
             <div id="tab" class="device">
@@ -28,12 +54,9 @@
                     <img src="../img/mobile_layout.jpg" alt="Mobile Layout">
                 </div>
             </div>
-            <div id="text" class="device">
-                <div class="screen">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </div>
-            </div>
         </div>
     </div>
 </body>
+<script type="text/javascript" src="./js/bower_components/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="./js/landing.js"></script>
 </html>
