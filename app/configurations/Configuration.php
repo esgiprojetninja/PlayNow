8145649bf89fa87b43bb2dec3e6d306d37e105c2
Lambot <lambot.rom@gmail.com>
2016-02-29 12:54:03 +0100

    fix master

--- app/configurations/Configuration.php
+++ app/configurations/Configuration.php
@@ -4,11 +4,7 @@ namespace Playnow\app\configurations;
 
 class Configuration implements ConfigurationInterface
 {
-<<<<<<< HEAD
     public function __construct($arrayConfig) {
 
     }
-=======
-    
->>>>>>> aacc8f52c84497b50e329842ac2bb2910187cf06
 }
